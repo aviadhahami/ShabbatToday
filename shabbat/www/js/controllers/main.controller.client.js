b7e56008a82ea2da8749afa04e1362086f29af11
Aviad Hahami <aviadhahami@gmail.com>
2015-09-13 01:39:04 +0300

    added ctlrt

---
+++ shabbat/www/js/controllers/main.controller.client.js
@@ -0,0 +1,16 @@
+app.controller("mainController", function($scope, $cordovaMedia, $ionicLoading) {
+
+  $scope.play = function(src) {
+    var media = new Media(src, null, null, mediaStatusCallback);
+    $cordovaMedia.play(media);
+  }
+
+  var mediaStatusCallback = function(status) {
+    if(status == 1) {
+      $ionicLoading.show({template: 'Loading...'});
+    } else {
+      $ionicLoading.hide();
+    }
+  }
+
+});
