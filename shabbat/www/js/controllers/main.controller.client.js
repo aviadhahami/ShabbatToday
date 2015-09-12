ae91f5e6bd0118bccdf0c84a226165cc24fb64ed
Aviad Hahami <aviadhahami@gmail.com>
2015-09-13 02:34:09 +0300

    Done with all evn shit. need to add sound files

--- shabbat/www/js/controllers/main.controller.client.js
+++ shabbat/www/js/controllers/main.controller.client.js
@@ -1,6 +1,6 @@
 app.controller("mainController", function($scope, $cordovaMedia, $ionicLoading) {
-
-  $scope.play = function(src) {
+  console.log('controller running');
+  var play = function(src) {
     var media = new Media(src, null, null, mediaStatusCallback);
     $cordovaMedia.play(media);
   }
@@ -11,6 +11,19 @@ app.controller("mainController", function($scope, $cordovaMedia, $ionicLoading) 
     } else {
       $ionicLoading.hide();
     }
+  }
+  $scope.playSound = function(){
+    console.log('played sound');
+    var src_string = isAndroid ? androidSourceString +getSoundSrc() : getSoundSrc();
+    //play(src_string);
+    play(getSoundSrc());
+  };
+  var isAndroid = function(){
+    return ionic.Platform.isAndroid()
+  };
+  var androidSourceString = '/android_asset/';
+  var getSoundSrc = function(){
+    return 'http://www.stephaniequinn.com/Music/Commercial%20DEMO%20-%2013.mp3';
   }
 
 });
