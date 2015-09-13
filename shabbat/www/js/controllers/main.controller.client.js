42323457c53cd128d0a365226edda7fe2c8dae0b
Aviad Hahami <aviadhahami@gmail.com>
2015-09-13 03:18:32 +0300

    Added MP3, let see if works

--- shabbat/www/js/controllers/main.controller.client.js
+++ shabbat/www/js/controllers/main.controller.client.js
@@ -28,14 +28,17 @@ app.controller("mainController", function($scope, $cordovaMedia, $ionicLoading,$
   var playSound = function(){
     console.log('played sound');
     var src_string = isAndroid ? androidSourceString +getSoundSrc() : getSoundSrc();
-    //play(src_string);
+    play(src_string);
   };
   var isAndroid = function(){
     return ionic.Platform.isAndroid()
   };
+
   var androidSourceString = '/android_asset/';
   var getSoundSrc = function(){
-    return 'http://www.stephaniequinn.com/Music/Commercial%20DEMO%20-%2013.mp3';
+    var fileName = 's1.mp3';
+    var pathString = 'www/mp3/';
+    return pathString + fileName;
   }
 
 });
