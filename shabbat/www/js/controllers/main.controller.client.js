c2dbfb818bdc642f196f1514873e47e17e98d366
Aviad Hahami <aviadhahami@gmail.com>
2015-09-13 03:00:28 +0300

    Fixed some stuff, need to fix text

--- shabbat/www/js/controllers/main.controller.client.js
+++ shabbat/www/js/controllers/main.controller.client.js
@@ -1,8 +1,9 @@
-app.controller("mainController", function($scope, $cordovaMedia, $ionicLoading) {
+app.controller("mainController", function($scope, $cordovaMedia, $ionicLoading,$timeout) {
   console.log('controller running');
+  $scope.dayToday = '?שבת היום';
   var play = function(src) {
-    var media = new Media(src, null, null, mediaStatusCallback);
-    $cordovaMedia.play(media);
+    //var media = new Media(src, null, null, mediaStatusCallback);
+    //$cordovaMedia.play(media);
   }
 
   var mediaStatusCallback = function(status) {
@@ -11,12 +12,23 @@ app.controller("mainController", function($scope, $cordovaMedia, $ionicLoading) 
     } else {
       $ionicLoading.hide();
     }
+  };
+
+  $scope.playSequence = function(){
+    updateString();
+    $timeout(restoreString, 200);
+    playSound();
+  };
+  var updateString = function(){
+    $scope.dayToday = '!שבת היום';
+  };
+  var restoreString = function(){
+    $scope.dayToday = '?שבת היום';
   }
-  $scope.playSound = function(){
+  var playSound = function(){
     console.log('played sound');
     var src_string = isAndroid ? androidSourceString +getSoundSrc() : getSoundSrc();
     //play(src_string);
-    play(getSoundSrc());
   };
   var isAndroid = function(){
     return ionic.Platform.isAndroid()
